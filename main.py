c65044021f2e706c43018ffc85ae99ab70684397
gosia4 <93672508+gosia4@users.noreply.github.com>
2023-11-06 22:07:36 +0100

    GET added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory, abort
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
@@ -9,12 +9,23 @@ def upload_page():
     return render_template('upload.html')
 
 
-@app.route('/uploader', methods=['GET', 'POST'])
+@app.route('/uploader', methods=['POST'])
 def upload_file():
-    if request.method == 'POST':
-        f = request.files['file']
-        f.save(secure_filename(f.filename))
-        return 'file uploaded successfully'
+    f = request.files['file']
+    f.save(secure_filename(f.filename))
+    return 'file uploaded successfully'
+
+
+@app.route('/get_file', methods=['GET'])
+def get_file():
+    file_name = request.args.get('file_name')
+    if file_name:
+        try:
+            return send_from_directory('', file_name)
+        except FileNotFoundError:
+            abort(404)
+    else:
+        abort(404)
 
 
 if __name__ == '__main__':
